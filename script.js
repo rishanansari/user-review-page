8737b7c89579fd0efd3227d7af03c9b80a224b64
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-06-18 10:40:57 +0530

    js added

---
+++ script.js
@@ -0,0 +1,212 @@
+//Array of objec  
+const reviews=[
+    {
+    "name": "Amos Beahan",
+    "job": "Legacy Operations Engineer",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/471.jpg",
+    "text": "Rerum ullam debitis fuga totam sit inventore. Similique possimus dicta officia. Voluptate sunt quibusdam ut unde porro. Fuga ipsum quia et iure inventore provident illo. Qui expedita exercitationem. Iure deleniti maiores sapiente neque et ut quae sequi occaecati. Consequuntur fugit eaque ducimus ullam nobis facere enim quis quo. Cupiditate repellendus doloremque et delectus. Nisi natus voluptatibus. Aut reiciendis nam in vero nostrum fuga aspernatur. Tempora dicta perspiciatis adipisci ad voluptate odit reiciendis laborum. Enim totam sed quas nemo fuga voluptatibus. Quaerat repellendus beatae neque odio non dolores dolorem placeat sed. Aliquam at explicabo magnam totam sed. Doloremque repellendus et eveniet dolores et occaecati.",
+    "id": "1"
+    },
+    {
+    "name": "Mattie Hagenes",
+    "job": "Principal Paradigm Coordinator",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/406.jpg",
+    "text": "Rerum consectetur quia ea vitae ea. Voluptate iste tempora quibusdam. Voluptas similique distinctio labore architecto ut. Perspiciatis quisquam deserunt aliquid ratione sint. Possimus iusto nemo molestiae quidem mollitia. Expedita est maxime. Voluptates delectus quis id ipsum et autem qui. Totam assumenda omnis. Assumenda ratione minus nesciunt consequatur animi aut rerum. Sed dolorem maiores veritatis laudantium vitae dolores quisquam. Maiores unde aliquid et a non labore et. Id laboriosam debitis sit vel maiores. Minus magnam qui omnis laborum assumenda error consectetur voluptatem est. Culpa ut et. Voluptas modi ad qui earum. Eum nobis sit maiores. Autem nostrum molestiae dolorem tempora eos ea inventore sapiente.",
+    "id": "2"
+    },
+    {
+    "name": "Mr. Mark Marks",
+    "job": "Lead Infrastructure Officer",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/528.jpg",
+    "text": "Excepturi fugiat nisi quibusdam. Ad veniam ex nihil et vel velit ut dolores in. Molestias magni expedita nesciunt quidem aperiam blanditiis commodi quis. Cumque tempore quo ex. Id nihil ut modi ut ad libero velit. Voluptatem officiis ipsam ducimus voluptatem. Nobis sed iste culpa perspiciatis hic. Voluptas et et dolorum itaque est ut. Itaque voluptatibus dolorem vero ad nobis sint itaque et. Consequatur quia quaerat magni natus aliquam cupiditate sint aut. Voluptatum nam nam ratione modi temporibus sit excepturi iure. Suscipit numquam veniam atque facilis maxime eligendi quod. Aut et voluptatem enim eveniet totam quasi deserunt. Et consectetur molestiae laborum fugiat molestiae voluptatum. Alias id quo soluta. Voluptas in corrupti molestiae nam aut officiis quas est. Ducimus deserunt est assumenda ipsam sit nisi iure delectus.",
+    "id": "3"
+    },
+    {
+    "name": "Shawna Bauch",
+    "job": "Internal Brand Supervisor",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/892.jpg",
+    "text": "Libero dolorum tenetur explicabo earum voluptatibus possimus. Excepturi optio sed ut harum id eveniet porro voluptas. Ipsa dolorem reprehenderit sit. Vitae non sunt explicabo. Exercitationem non et ducimus. Incidunt occaecati animi libero voluptatem sed nemo in consequatur. Veniam ipsa itaque. Qui natus sed odio eligendi ea. Labore nam quia voluptatem ipsa sed distinctio voluptatem aliquid. Quam maxime illum perferendis. Et fuga quo a. Repellendus molestias quasi cupiditate dignissimos natus id. Velit et velit est dolorum molestiae quis. Delectus maiores repudiandae. Maiores necessitatibus commodi. Facere et sapiente ipsam culpa cumque.",
+    "id": "4"
+    },
+    {
+    "name": "Emily Botsford",
+    "job": "Internal Creative Liaison",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/233.jpg",
+    "text": "Reprehenderit accusantium distinctio ex officia. Consequatur harum aspernatur nostrum officiis ut rerum sunt itaque. Ipsum ea molestiae vero ut aut ut. Harum repellendus sunt modi rerum quae culpa consequatur velit. Laboriosam aut quia quo earum. Veritatis accusantium modi nobis mollitia ipsa sit est alias qui. Qui molestiae quo aliquid culpa eligendi. Voluptas labore at nemo sapiente ea quod. Quis occaecati quae numquam. Ullam expedita odit ipsum aliquam debitis incidunt ducimus eveniet. Quibusdam sit eos aspernatur quas. Minima est nostrum tempora perspiciatis aut. Est et dolor veniam ab sint ut fuga alias. Aut sed quos sunt enim est ut. Voluptate minus qui voluptas.",
+    "id": "5"
+    },
+    {
+    "name": "Henry Marvin",
+    "job": "National Group Architect",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/266.jpg",
+    "text": "Id neque aut veritatis. Aliquid dolorem unde enim laudantium repudiandae repellat esse fugiat. Similique similique corporis hic ut. Velit in sed. Quisquam rerum sint corrupti sequi. Totam in officia dignissimos et. Alias cupiditate quos earum est explicabo exercitationem blanditiis recusandae. Incidunt ut asperiores possimus accusantium adipisci et molestiae. Natus sint quia sapiente ex qui debitis perferendis minus. Reiciendis sunt et aperiam ducimus in rerum. Commodi sapiente rerum voluptatem aut accusantium neque. Deleniti ad ut harum aut quia inventore dolor distinctio dolores. Vel ab velit labore dolor. Ut incidunt odio nihil est. Sunt sed assumenda odio libero esse. Corporis autem consequatur natus.",
+    "id": "6"
+    },
+    {
+    "name": "Cathy Feest",
+    "job": "Chief Assurance Analyst",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/760.jpg",
+    "text": "Vitae atque fugit inventore quae dolorum. Inventore nam ratione fuga aperiam ut temporibus aut repellendus ut. Dolores quia ea et. Sit est sed. Repellendus voluptate aut suscipit laudantium et nulla nostrum. Eum voluptatem id. Unde adipisci est sint dolore quibusdam reprehenderit. Quas vel ab id molestias. Molestias ut voluptatem labore qui. Voluptatem illum molestiae. Adipisci ex at. Ea ipsa nostrum iste sint et dolor.",
+    "id": "7"
+    },
+    {
+    "name": "Kirk Conn",
+    "job": "Product Research Analyst",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/214.jpg",
+    "text": "Qui laborum dolorem cum enim ut qui perspiciatis minus. Illo voluptatem consectetur et nobis et. Temporibus sed porro cum. Sit molestias et incidunt. Qui est fugit incidunt explicabo. Qui repellat aspernatur doloremque quae non. Dolorem accusamus amet labore nostrum sunt maxime. Quia quos et nam facere repellendus et. Recusandae nihil iure. Blanditiis excepturi aut aperiam aspernatur eum labore qui. Quae quam praesentium repellendus enim. Autem hic facilis iusto assumenda ut omnis dolor cumque. Consequuntur sunt labore ut expedita. Neque consequuntur id tenetur ut et.",
+    "id": "8"
+    },
+    {
+    "name": "Jeannie Brekke",
+    "job": "Dynamic Mobility Director",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1118.jpg",
+    "text": "Voluptas assumenda autem velit rerum unde occaecati modi animi similique. Id qui ut magnam laborum et omnis dolore error aut. Ipsum et illo nisi sequi quia quos consectetur. Dolores et id voluptas. Nisi et consequatur in tenetur eos. Et quasi dolor commodi ut id porro porro voluptatibus. Et sit quia aliquam vel velit voluptatem dicta. Sunt possimus consequatur non. Dolores quae aut voluptatem sit. Voluptate id ut asperiores fuga voluptatem sint et dolorem. Placeat dolores quia earum. Est ut et consequuntur ut quam. Unde aut nostrum. Fugit et est dicta esse. Laudantium fuga assumenda sint laudantium fugit impedit deserunt dolores omnis.",
+    "id": "9"
+    },
+    {
+    "name": "Shannon Hilpert",
+    "job": "Central Markets Architect",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/920.jpg",
+    "text": "Aut ex aspernatur magnam error nobis nemo neque. Est at et aut omnis quis voluptatem. Recusandae numquam reiciendis aut non voluptatem beatae. Odio ea eius non nisi rerum aliquam. Omnis esse voluptates quisquam ut sequi ipsam quia. Accusantium et autem nam voluptatem cupiditate dignissimos eius vel. Repellat labore error. Inventore harum sequi sequi doloribus occaecati. Distinctio in asperiores et fugiat sint qui. Consequuntur ea ut voluptates voluptatibus temporibus eaque. Molestiae commodi est id eligendi non perferendis. Labore dolores ut quibusdam sit nihil in recusandae similique. Magnam vero nostrum doloremque dolor distinctio libero.",
+    "id": "10"
+    },
+    {
+    "name": "Lee Kiehn",
+    "job": "Corporate Optimization Executive",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/945.jpg",
+    "text": "Dicta veritatis vitae voluptates nulla dolorum corrupti alias maiores quos. Aliquam voluptatibus modi molestias numquam. Impedit id temporibus exercitationem est optio eligendi ex tempore. Provident tempore saepe officia sapiente consequatur necessitatibus dolores. Sequi ut rerum. Odit dolorem sed dolores hic. Earum et quaerat ut perspiciatis ut illo qui. Aliquam est quia id optio et sint. Omnis eius consectetur non blanditiis possimus. Minus qui id repellendus sed in. Illum distinctio reiciendis et. Minus id expedita. Cumque fuga animi quidem cupiditate minima. Non tenetur optio.",
+    "id": "11"
+    },
+    {
+    "name": "Vanessa Bosco",
+    "job": "Human Metrics Specialist",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/502.jpg",
+    "text": "Ullam magni aut totam rerum aut omnis quia velit. Quasi at quis id voluptatem. Magnam voluptas vero ut dolore magni nihil. Architecto maxime eveniet amet aut nobis. Eum non nulla eveniet sed hic repellendus aut rerum. Quia veritatis placeat ab autem blanditiis est dignissimos veniam. Nostrum occaecati illo fugit dolore sed mollitia. Nemo consequatur libero odit est delectus nihil. Delectus nostrum adipisci velit. Enim laudantium harum qui ratione rem tempore aut assumenda omnis. Eos aut ab. Sed est sit dicta consectetur earum qui sunt. At laboriosam dolores quae voluptatum adipisci hic dolorem possimus sed. Consequatur ratione aut occaecati eligendi aut suscipit.",
+    "id": "12"
+    },
+    {
+    "name": "Velma Emard",
+    "job": "Senior Implementation Specialist",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/412.jpg",
+    "text": "Eaque non quia. Architecto laudantium recusandae. Blanditiis aut incidunt est fugit debitis aut et. Nostrum possimus dolores quis molestiae hic aut vel. Et rerum itaque culpa et. Similique in reiciendis corrupti ea tempore dolor nihil nostrum. Amet qui quo recusandae corporis quia aut. Consequatur aperiam harum inventore voluptatibus sunt ut et quasi unde. Temporibus nobis quia est similique explicabo et. Corporis quos illum. Eveniet libero dolores ut libero nihil et quasi corrupti.",
+    "id": "13"
+    },
+    {
+    "name": "Diana Jacobson",
+    "job": "Regional Infrastructure Supervisor",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1083.jpg",
+    "text": "Saepe fuga tempora repudiandae est. Aperiam magnam labore ut consequatur maiores et. Doloremque maiores minima hic ut dolor. Commodi aliquam mollitia pariatur occaecati expedita ab ipsa consequuntur. Corrupti cupiditate temporibus porro voluptas dolorum sint dolor ut consequatur. Et laborum iure ad. Neque laborum vitae consequatur similique vel. Quas eveniet et est sed sequi. Odio qui iste sapiente voluptas est assumenda ea vitae numquam. Et voluptatem vitae non aspernatur aut. Sapiente consequatur ea et dolor voluptatum fugit. Ut assumenda ut illum voluptatem eum dicta. Dicta et consequatur dolores pariatur eius doloribus rem. Sapiente ducimus magnam enim harum.",
+    "id": "14"
+    },
+    {
+    "name": "Adrian Hartmann",
+    "job": "Future Factors Strategist",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1078.jpg",
+    "text": "Et iusto inventore ea. Qui qui harum nihil. Ipsa commodi harum illo. Quia omnis iusto aut non at quas at. Sit iste cumque sunt. At esse dolores quis magni repellat iusto laborum enim cum. Et cum qui sapiente et. Facilis quo id sit quisquam rerum illo ut sunt. Tempore beatae ut suscipit deleniti dolor labore laborum voluptatem a. Ullam cupiditate quia et. Dolor laborum minima blanditiis nostrum nemo. Distinctio est nisi quia nisi ullam labore repudiandae perferendis. Provident quod quas molestiae dolorem. Ad voluptate quia. Nam dolores expedita voluptatem hic earum cum libero. Quibusdam non aut dolore.",
+    "id": "15"
+    },
+    {
+    "name": "Aubrey Herman",
+    "job": "Internal Data Assistant",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/58.jpg",
+    "text": "Fuga voluptas dignissimos dolor. Quas qui consequatur sed vero quo sint ratione. Eius culpa ea magnam ducimus est occaecati voluptates. Sint nisi et vel sit. Aut qui facere voluptatem autem enim vero architecto. Blanditiis sed id voluptatem quidem harum quia vitae nihil. Aliquam assumenda nihil fugiat hic. Est quia non voluptatem quis quia et facilis. Fuga aut esse totam vel illo inventore magni tenetur veritatis. Repellat tenetur tempora consequuntur quod sed. Deleniti deserunt qui ea tempora maiores voluptatem amet. Et architecto non aspernatur id quod voluptatem. Est natus quam est.",
+    "id": "16"
+    },
+    {
+    "name": "Ella Lang",
+    "job": "Senior Interactions Specialist",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/95.jpg",
+    "text": "Explicabo tempora sapiente recusandae cumque est consequatur et. Non officia totam qui. Molestias nemo saepe qui. Voluptatem et reprehenderit. Sunt aut libero tenetur iste doloremque earum. Nemo voluptate cumque maiores. Perspiciatis expedita eos provident totam qui quae accusantium. Nihil labore ipsum nemo blanditiis nostrum aliquid aperiam soluta dolores. Libero quos reiciendis quasi id quam reprehenderit. Qui soluta molestias dolorum et aut.",
+    "id": "17"
+    },
+    {
+    "name": "Charles Williamson",
+    "job": "Lead Security Liaison",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/971.jpg",
+    "text": "Laboriosam aut dolorum natus ipsum neque omnis voluptatibus debitis reprehenderit. Voluptas velit praesentium necessitatibus qui. Dolorum autem quod non ut. Consequatur dolores labore est odit qui quia nulla. Ea alias assumenda esse et suscipit explicabo dolor quia. Quia non voluptatem eos nesciunt. Delectus ut nam repellat ducimus quibusdam et iste. Eos alias illo. Id facere fugit adipisci exercitationem minima officiis voluptatem. Ipsum amet harum. Perferendis quis sed molestiae. Explicabo doloremque non sunt est.",
+    "id": "18"
+    },
+    {
+    "name": "Alice Romaguera",
+    "job": "Customer Optimization Consultant",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1064.jpg",
+    "text": "Debitis blanditiis quo cum quod qui repellendus ducimus qui veritatis. Iste recusandae dolorum nisi recusandae repellendus illo. Ea numquam odio. Officia impedit saepe voluptatem neque. Ut aliquam tenetur magni iusto accusamus alias. Est eaque quasi praesentium. Quia alias commodi. Quis tempore quis dolores eveniet sit sit repudiandae. Est quo velit. Totam quisquam ducimus est pariatur. Quo veritatis ex dignissimos incidunt facere sapiente. Voluptatibus minima placeat laudantium. Ipsam neque suscipit. Neque qui et nostrum.",
+    "id": "19"
+    },
+    {
+    "name": "Johnathan Marquardt Jr.",
+    "job": "Forward Research Executive",
+    "img": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1049.jpg",
+    "text": "Repellat quasi eligendi vel sequi. Id id aut doloremque. Eveniet nihil omnis perferendis similique sed eveniet rerum unde et. Temporibus nisi ipsum blanditiis nam. Ut delectus sequi porro id. Consequatur praesentium commodi molestiae est quaerat harum commodi a. Consequatur facere quas cum accusantium enim doloribus aliquam maiores. Sequi dolorem a voluptatem eius occaecati sunt temporibus enim. Eos cumque voluptatum non aut perferendis omnis. Iusto quod et cumque. Ab officia est excepturi quam deleniti reprehenderit aut quia quo. Perferendis rerum tempore ipsa pariatur qui. Eveniet tempore ullam commodi dolorem alias nihil aut mollitia. Iusto aut temporibus qui dolor aspernatur. Qui dolores exercitationem.",
+    "id": "20"
+    }
+    ]
+
+
+
+//Select user target element
+const img=document.getElementById("person-img");
+const author=document.getElementById("author");
+const job=document.getElementById("job");
+const info=document.getElementById("info");
+
+
+//lets select button
+const prevBtn=document.querySelector(".prev-btn");
+const nextBtn=document.querySelector(".next-btn");
+const randomBtn=document.querySelector(".random-btn");
+
+
+//Try to load the data for first time 
+let currentItem=0;
+window.addEventListener("DOMContentLoaded",()=>{
+   const item=reviews[currentItem];//first data of the array of objects
+   console.log(item);
+   img.src=item.img;
+   author.innerHTML=item.name;
+   job.innerHTML=item.job;
+   info.innerHTML=item.text;
+})
+
+//create a function showUserReview based on button 
+//is to print and load other data infor
+function showUserReview(person){
+    const item=reviews[person];//reviews[0]
+    console.log(currentItem);
+   img.src=item.img;
+   author.innerHTML=item.name;
+   job.innerHTML=item.job;
+   info.innerHTML=item.text;
+}
+
+
+//Logic for next user when user clicks on button
+nextBtn.addEventListener("click",()=>{
+    //increment the current position +1
+    currentItem++;//20
+    //is user reached to the end of the object or last ->it should be reset to 0
+    // 20>19
+    if(currentItem>reviews.length-1){
+        currentItem=0;
+    }
+    showUserReview(currentItem);
+})
+
+//Logic for previous user when user click on button 
+prevBtn.addEventListener("click", ()=>{
+    currentItem--;
+
+    if(currentItem<0){
+        currentItem=reviews.length-1;//point current item to last index if user <0
+    }
+
+    showUserReview(currentItem);
+})
+
+
+//get the logic of random user
+randomBtn.addEventListener("click",()=>{
+    currentItem=Math.trunc(Math.random()*reviews.length);
+    console.log(currentItem);
+    showUserReview(currentItem);
+});
